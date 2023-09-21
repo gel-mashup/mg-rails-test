0e25c56d66a617eaf40611a00579b32155c0d191
“23nith” <zenithmanuel@gmail.comgit config --global user.email ZENITHMANUEL@GMAIL.com>
2023-09-21 23:34:18 +0800

    Fix descending sort

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,12 +1,20 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
-  before_action :set_items
+  before_action :set_items, only: [:index]
+  before_action :retreive_items, only: [:order_by_fruit]
 
-  def index; end
+  def index;
+    puts "INDEX ACTION TRIGGERED"
+  end
 
   def order_by_fruit
-    @list_items = @list_items.sort
-    @list_items = @list_items.reverse
+    @list_items = session[:list_items].to_h
+    if(@list_items.keys.first > @list_items.keys.last)
+      @list_items = @list_items.sort
+    else
+      @list_items = @list_items.sort.reverse
+    end
+    session[:list_items] = @list_items.to_h
     render 'basket_case/index'
   end
 
@@ -27,6 +35,14 @@ class BasketCaseController < ApplicationController
     end
 
     def set_items
+      puts "SET ITEMS TRIGGERED"
       @list_items = @fruits
+      session[:list_items] = @fruits
+    end
+
+    def retreive_items
+      puts "RETRIEVE ITEMS TRIGGERED"
+      @list_items = session[:list_items]
     end
 end
+
