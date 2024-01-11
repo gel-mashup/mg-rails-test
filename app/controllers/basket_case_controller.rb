4ef459f0a694f7a3ee54e680cd6ab764c33fce3d
Catherine Liwag <liwagcath@gmail.com>
2024-01-11 11:57:13 +0800

    implemented asc/desc order

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,16 +1,20 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
   before_action :set_items
+  before_action :build_def_order_param
 
   def index; end
 
   def order_by_fruit
     @list_items = @list_items.sort
+    @list_items = @list_items.reverse if params.has_key?(:order) && params[:order] == "desc"
     render 'basket_case/index'
   end
 
   def order_by_amount
     @list_items = @list_items.sort_by(&:last)
+    @list_items = @list_items.reverse if params.has_key?(:order) && params[:order] == "desc"
+
     render 'basket_case/index'
   end
 
@@ -26,5 +30,9 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def build_def_order_param
+      @order = params[:order] == "asc" ? "desc" : "asc"
     end
 end
