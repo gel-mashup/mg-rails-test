c287045341f724cd42454446f9c29032d3cbc8de
Catherine Liwag <liwagcath@gmail.com>
2024-01-11 11:49:00 +0800

    fixed sorter

--- app/controllers/basket_case_controller.rb
+++ app/controllers/basket_case_controller.rb
@@ -1,17 +1,19 @@
 class BasketCaseController < ApplicationController
   before_action :set_fruits
   before_action :set_items
+  before_action :build_def_order_param
 
   def index; end
 
   def order_by_fruit
     @list_items = @list_items.sort
-    @list_items = @list_items.reverse
+    @list_items = @list_items.reverse if params.has_key?(:order) && params[:order] == "desc"
     render 'basket_case/index'
   end
 
   def order_by_amount
-    # code here
+    @list_items = @list_items.sort_by(&:last)
+    @list_items = @list_items.reverse if params.has_key?(:order) && params[:order] == "desc"
 
     render 'basket_case/index'
   end
@@ -29,5 +31,9 @@ class BasketCaseController < ApplicationController
 
     def set_items
       @list_items = @fruits
+    end
+
+    def build_def_order_param
+      @order = params[:order] == "asc" ? "desc" : "asc"
     end
 end
